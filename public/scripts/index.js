3bfef1d967aca01d1b403a81f02fbde11c2ce7be
robertopeiro12 <robertopeiro12@gmail.com>
2025-04-10 10:15:52 -0600

    Refactor login function to improve variable scope and organization

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,7 +1,12 @@
 // Este archivo contiene el código JavaScript para la página de inicio de sesión
+const boton = document.getElementById("btn-login");
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+
 const login = () => {
     if (username.value === "admin" && password.value === "1234") {
         alert("Bienvenido");
+        sessionStorage.name = username.value;
         window.location = "./pages/tags.html";
     }
     else {
@@ -11,7 +16,5 @@ const login = () => {
     
 };
 
-const boton = document.getElementById("btn-login");
-const username = document.getElementById("username");
-const password = document.getElementById("password");
+
 boton.addEventListener("click", login);
