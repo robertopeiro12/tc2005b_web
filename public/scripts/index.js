41299d7bcf42d79041cfc18631b1616bddb1c5ac
robertopeiro12 <robertopeiro12@gmail.com>
2025-05-15 09:09:26 -0600

    Refactor login function to implement asynchronous credential validation and improve session handling

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -3,16 +3,30 @@ const boton = document.getElementById("btn-login");
 const username = document.getElementById("username");
 const password = document.getElementById("password");
 
-const login = () => {
-    if (username.value === "admin" && password.value === "1234") {
-        alert("Bienvenido");
-        sessionStorage.name = username.value;
+const  login =  async () => {
+    // validar credenciales
+    const credentials = {username:username.value, password: password.value}
+    const data = await fetch("http://localhost:5001/login", {
+        method:"POST",
+        headers:{"content-type":"application/json"}, 
+        body: JSON.stringify(credentials)});
+
+    const res = await data.json();
+    //console.log(username.value + " " + password.value);
+    // Si el login es correcto
+    if (res.isLogin) {
+        sessionStorage.setItem("name", res.user.name);
+        sessionStorage.setItem("id", res.user.id);
         window.location = "./pages/profile.html";
+    }else {
+        // Si el login es incorrecto
+    alert("Credenciales incorrectas");
+
     }
-    else {
-        alert("Usuario o contraseña incorrecta");
-    }
+   
     
+    
+
     
 };
 
